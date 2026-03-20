69fe6748f5b24e84d84cf71024a3f3d66707a35a
SwiftFranek <127611343+02franek@users.noreply.github.com>
2026-03-20 13:49:51 +0100

    Zadanie_1/3.5

--- zadanie_1/bubblesort.pas
+++ zadanie_1/bubblesort.pas
@@ -5,6 +5,7 @@ type
 
 var
 	RandNumArray : TNumArray;
+	ItemCount : integer;
 
 procedure PrintArray(arr : TNumArray);
 var
@@ -27,8 +28,30 @@ begin
 	end;
 end;
 
+procedure SortArray(var arr : TNumArray; count : integer);
+var
+	i, j, tmp : integer;
 begin
+	for i := 1 to count - 1 do
+	begin
+		for j := 1 to count - i do
+		begin
+			if arr[j] > arr[j + 1] then
+			begin
+				tmp := arr[j];
+				arr[j] := arr[j + 1];
+				arr[j + 1] := tmp;
+			end;
+		end;
+	end;
+end;
+
+begin
+	ItemCount := 50;
 	Randomize;
 	PopulateArray(RandNumArray);
+	PrintArray(RandNumArray);
+	WriteLn;
+	SortArray(RandNumArray, ItemCount);
 	PrintArray(RandNumArray);
 end.
