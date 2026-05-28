8097035a4660f805fc86914c2cf2ae23644ed913
SwiftFranek <127611343+02franek@users.noreply.github.com>
2026-05-28 17:59:25 +0200

    Zadanie_5/3.5

--- zadanie_5/client/src/components/Cart.tsx
+++ zadanie_5/client/src/components/Cart.tsx
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Cart = () => {
-  return <div>Koszyk</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h2>Twój koszyk</h2>
+      <p>Koszyk jest pusty</p>
+      <h3>Suma: 0</h3>
+      <button onClick={() => navigate("/payments")}>
+        Przejdź do płatności
+      </button>
+    </div>
+  );
 };
 
 export default Cart;
